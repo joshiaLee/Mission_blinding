5789d6e3afde7eb90cfcd74c5ddcc62e55f2307e
joshiaLee <hhhjs0133@naver.com>
2024-01-16 00:42:37 +0900

    리팩토링

--- lion/src/main/java/com/example/lion/LionApplication.java
+++ lion/src/main/java/com/example/lion/LionApplication.java
@@ -3,6 +3,7 @@ package com.example.lion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 연습
 @SpringBootApplication
 public class LionApplication {
 
